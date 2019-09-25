22264ce8cc2b7afa1e4af66cf0498dcb8c424588
Franco21198 <francoromanazzi@hotmail.com>
2019-09-25 16:23:13 -0300

    Creo schema + creo tabla Usuarios

---
+++ data/script_creación_inicial.sql
@@ -0,0 +1,37 @@
+USE GD2C2019
+GO
+
+------------------------------------------------
+--            DROP TABLES
+------------------------------------------------
+
+IF OBJECT_ID('LOS_BORBOTONES.Usuarios') IS NOT NULL DROP TABLE LOS_BORBOTONES.Usuarios
+GO
+
+------------------------------------------------
+--            DROP SCHEMA
+------------------------------------------------
+
+IF SCHEMA_ID('LOS_BORBOTONES') IS NOT NULL DROP SCHEMA LOS_BORBOTONES
+GO
+
+------------------------------------------------
+--            CREATE SCHEMA
+------------------------------------------------
+
+CREATE SCHEMA LOS_BORBOTONES
+GO
+
+------------------------------------------------
+--            CREATE TABLES
+------------------------------------------------
+
+CREATE TABLE LOS_BORBOTONES.Usuarios (
+	id_usuario INT IDENTITY(1,1) NOT NULL,
+	username NVARCHAR(50) NOT NULL,
+	[password] NVARCHAR(255) NOT NULL,
+	habilitado BIT DEFAULT 1 NOT NULL,
+	motivo_deshabilitacion NVARCHAR(50) DEFAULT NULL,
+	cant_intentos_fallidos TINYINT DEFAULT 0 NOT NULL,
+	PRIMARY KEY (id_usuario)
+)
