cdfbb3e1b3e29edf9117659bea5b0260e54ec338
LizaTheAnnihilator <gyupanquisalvatierra@gmail.com>
2019-10-25 23:02:57 -0300

    termino prosures de listado estatico/falta codificar

--- ListadoEstatico.sql
+++ ListadoEstatico.sql
@@ -1,17 +1,35 @@
 use GD2C2019;
 go
 --Top 5 mejores ofertas
-select top 5 p.id_proveedor, Max(o.precio_de_lista-o.precio_en_oferta / o.precio_de_lista ) as Porcentaje
-from LOS_BORBOTONES.Proveedores p 
-left join LOS_BORBOTONES.Ofertas o on (p.id_proveedor = o.id_proveedor)
---where fecha
-group by p.id_proveedor
-
+DROP PROCEDURE ProveedoresMayorDescuento;
+go
+CREATE PROCEDURE ProveedoresMayorDescuento (@anio int,@semestre int) AS
+BEGIN
+	select top 5 p.id_proveedor, Max(o.precio_de_lista-o.precio_en_oferta / o.precio_de_lista ) as Porcentaje
+	from LOS_BORBOTONES.Proveedores p 
+	left join LOS_BORBOTONES.Ofertas o on (p.id_proveedor = o.id_proveedor)
+	where year(o.fecha_publicacion) = @anio AND month(o.fecha_publicacion) / 7 = @semestre -1
+	group by p.id_proveedor
+	order by 2 desc
+END
+go
+execute ProveedoresMayorDescuento 2020, 1
+execute ProveedoresMayorDescuento 2020, 2
+go
+-------------------
 
 --Top 5 Mayor Facturacion
-select top 5 id_proveedor, sum(precio_en_oferta*cant_unidades) as Facturacion 
-from LOS_BORBOTONES.Ofertas o 
-JOIN LOS_BORBOTONES.Compras c on (o.codigo_oferta = c.codigo_oferta)
---where fecha
-group by id_proveedor
-order by 2 desc
+DROP PROCEDURE ProveedoresMayorFacturacion;
+go
+CREATE PROCEDURE ProveedoresMayorFacturacion (@anio int,@semestre int) AS
+BEGIN
+	select top 5 id_proveedor, sum(precio_en_oferta*cant_unidades) as Facturacion 
+	from LOS_BORBOTONES.Ofertas o 
+	JOIN LOS_BORBOTONES.Compras c on (o.codigo_oferta = c.codigo_oferta)
+	where year(c.fecha) = @anio AND month(c.fecha) / 7 = @semestre -1 
+	group by id_proveedor
+	order by 2 desc
+END
+go
+execute ProveedoresMayorFacturacion 2020, 1
+execute ProveedoresMayorFacturacion 2020, 2
