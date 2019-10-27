147a382df90b5462063fb54f9fb78d79329e3350
LizaTheAnnihilator <gyupanquisalvatierra@gmail.com>
2019-10-27 17:34:52 -0300

    actualizo funcion + Prosedure

--- ListadoEstatico.sql
+++ ListadoEstatico.sql
@@ -1,5 +1,21 @@
 use GD2C2019;
 go
+--Funcion Semestre
+CREATE FUNCTION LOS_BORBOTONES.semestreFecha(@fecha DATE)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @semestre int;
+	IF ( MONTH(@fecha)<= 6 )
+		SET @semestre = 1
+	ELSE
+		SET @semestre = 2
+	RETURN @semestre;
+END
+go
+DROP FUNCTION LOS_BORBOTONES.semestreFecha;
+
+
 --Top 5 mejores ofertas
 DROP PROCEDURE ProveedoresMayorDescuento;
 go
@@ -8,7 +24,7 @@ BEGIN
 	select top 5 p.id_proveedor, Max(o.precio_de_lista-o.precio_en_oferta / o.precio_de_lista ) as Porcentaje
 	from LOS_BORBOTONES.Proveedores p 
 	left join LOS_BORBOTONES.Ofertas o on (p.id_proveedor = o.id_proveedor)
-	where year(o.fecha_publicacion) = @anio AND month(o.fecha_publicacion) / 7 = @semestre -1
+	where year(o.fecha_publicacion) = @anio AND LOS_BORBOTONES.semestreFecha(o.fecha_publicacion) = @semestre
 	group by p.id_proveedor
 	order by 2 desc
 END
@@ -16,7 +32,7 @@ go
 execute ProveedoresMayorDescuento 2020, 1
 execute ProveedoresMayorDescuento 2020, 2
 go
--------------------
+
 
 --Top 5 Mayor Facturacion
 DROP PROCEDURE ProveedoresMayorFacturacion;
@@ -26,7 +42,7 @@ BEGIN
 	select top 5 id_proveedor, sum(precio_en_oferta*cant_unidades) as Facturacion 
 	from LOS_BORBOTONES.Ofertas o 
 	JOIN LOS_BORBOTONES.Compras c on (o.codigo_oferta = c.codigo_oferta)
-	where year(c.fecha) = @anio AND month(c.fecha) / 7 = @semestre -1 
+	where year(c.fecha) = @anio AND LOS_BORBOTONES.semestreFecha(c.fecha) = @semestre
 	group by id_proveedor
 	order by 2 desc
 END
